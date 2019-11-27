65e0c6a042a594ebc88284e117db7cac544bd9b8
Hoku Kim <hoku@weehoo.com>
2019-11-27 08:26:34 -0800

    Added main activity app toolbar. Toolbar is empty; need to add tools to it.

--- app/src/main/java/com/weehoo/geenotes/MainActivity.java
+++ app/src/main/java/com/weehoo/geenotes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.weehoo.geenotes;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.MotionEvent;
