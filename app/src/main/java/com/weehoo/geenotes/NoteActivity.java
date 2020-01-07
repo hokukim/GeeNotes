03f0d9f36567c46c76327c056aef230564de7833
Hoku Kim <hoku@weehoo.com>
2020-01-06 17:46:50 -0800

    Note toolbar option for changing background. Magic number cleanup.

--- app/src/main/java/com/weehoo/geenotes/NoteActivity.java
+++ app/src/main/java/com/weehoo/geenotes/NoteActivity.java
@@ -43,7 +43,7 @@ import java.util.ArrayList;
 public class NoteActivity extends AppCompatActivity {
 
     public static final String NOTEBOOK_ID_EXTRA_KEY = "notebook_id_extra";
-    private final int MENU_TOOLS_GROUP_ORDER = 0;
+    private final int MENU_TOOLS_GROUP_ORDER = R.integer.note_menu_tools_group_order_min;
 
     private CanvasView mCanvasView;
     private ArrayList<ITool> mTools;
@@ -128,6 +128,32 @@ public class NoteActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Initialize the contents of the Activity's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.
+     *
+     * <p>This is only called once, the first time the options menu is
+     * displayed.  To update the menu every time it is displayed, see
+     * {@link #onPrepareOptionsMenu}.
+     *
+     * <p>The default implementation populates the menu with standard system
+     * menu items.  These are placed in the {@link Menu#CATEGORY_SYSTEM} group so that
+     * they will be correctly ordered with application-defined menu items.
+     * Deriving classes should always call through to the base implementation.
+     *
+     * <p>You can safely hold on to <var>menu</var> (and any items created
+     * from it), making modifications to it as desired, until the next
+     * time onCreateOptionsMenu() is called.
+     *
+     * <p>When you add items to the menu, you can implement the Activity's
+     * {@link #onOptionsItemSelected} method to handle them there.
+     *
+     * @param menu The options menu in which you place your items.
+     * @return You must return true for the menu to be displayed;
+     * if you return false it will not be shown.
+     * @see #onPrepareOptionsMenu
+     * @see #onOptionsItemSelected
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -266,6 +292,9 @@ public class NoteActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Loads the current notebook and page.
+     */
     private void loadNoteBook() {
         mNoteBookIndex = 0;
         mNotePageIndex = 0;
